55485696a4e181231b52c16cb7113f98e6f90f33
Adel <adel.udesign@gmail.com>
2023-07-31 03:20:47 +0330

    added weather SVGs

---
+++ src/js/weatherSVGs.js
@@ -0,0 +1,34 @@
+import morningDayCloudy from '/src/images/weather/morning-day-cloudy.svg';
+import morningDayRainy from '/src/images/weather/morning-day-rainy.svg';
+import morningDaySnow from '/src/images/weather/morning-day-snow.svg';
+import morningDayWind from '/src/images/weather/morning-day-wind.svg';
+
+import morningClear from '/src/images/weather/morning-weather/morning-clear.svg';
+import morningFoggy from '/src/images/weather/morning-weather/morning-foggy.svg';
+
+import dayClear from '/src/images/weather/day-weather/day-clear.svg'
+import dayEveningClear from '/src/images/weather/day-weather/day-evening-clear.svg'
+
+import nightClear from '/src/images/weather/night-weather/night-clear.svg'
+import nightRainy from '/src/images/weather/night-weather/night-rainy.svg'
+import nightSnow from '/src/images/weather/night-weather/night-snow.svg'
+
+
+const weatherSVGs = {
+    morningDayCloudy: morningDayCloudy,
+    morningDayRainy: morningDayRainy,
+    morningDaySnow: morningDaySnow,
+    morningDayWind: morningDayWind,
+
+    morningClear: morningClear,
+    morningFoggy: morningFoggy,
+
+    dayClear: dayClear,
+    dayEveningClear: dayEveningClear,
+
+    nightClear: nightClear,
+    nightRainy: nightRainy,
+    nightSnow: nightSnow,
+}
+
+export default weatherSVGs;
