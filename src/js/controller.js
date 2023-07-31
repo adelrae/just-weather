e8073a270606a42a44fb9286e139c398b56ecd5f
Adel <adel.udesign@gmail.com>
2023-08-01 03:13:58 +0330

    Control user's weather functionality

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,16 +1,30 @@
 import * as model from './model.js';
-import userWeatherInfoView from './views/userWeatherInfoView.js';
+import displayWeatherInfoView from './views/displayWeatherInfoView.js';
+import searchInputView from './views/searchInputView.js';
 
 import 'core-js';
 import 'regenerator-runtime/runtime.js';
 
 const controlUserWeatherInfo = async function() {
+    displayWeatherInfoView._renderLoader();
+
     await model.userLocationWeather();
     
-    userWeatherInfoView._render(model.state.userLocationWeather);
+    displayWeatherInfoView._render(model.state.userLocationWeather);
+}
+
+const controlSearchInput = async function() {
+    displayWeatherInfoView._renderLoader();
+
+    const query = searchInputView._getQuery();
+
+    await model.searchLocationWeather(query);
+
+    displayWeatherInfoView._render(model.state.searchLocationWeather);
 }
 
 const init = function() {
     controlUserWeatherInfo();
+    searchInputView._formHandler(controlSearchInput);
 };
 init();
