7891a5b0a6baf74bee966085d2ff3aebd72c4dad
Adel Raisi <adel.udesign@gmail.com>
2023-08-04 13:27:48 +0330

    js files

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 export const IP_INFO_API = 'https://ipinfo.io/json?';
 export const IP_INFO_TOKEN = '2751c7337c84da';
 
-export const WEATHER_API = 'http://api.weatherapi.com/v1/';
+export const WEATHER_API = 'https://api.weatherapi.com/v1/';
 export const WEATHER_API_KEY = 'd7b95daf1f3346d5be1131427233007';
 
 export const COUNTRIES_INFO_API = 'https://restcountries.com/v2/name/';
